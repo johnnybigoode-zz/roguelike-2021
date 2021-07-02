4ef2188f4ebc32a62a0f48242a5b826a21080622
João A. Marson <jamarson@gmail.com>
2021-07-01 21:32:52 -0300

    Procedural generated

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ from tcod.libtcodpy import map_get_height
 
 from engine import Engine
 from entity import Entity
-from game_map import GameMap
 from input_handlers import EventHandler
+from procgen import generate_dungeon
 
 def main() -> None:
     screen_width = 80
     screen_height = 50
 
     map_width = 80
-    map_get_height = 45
+    map_height = 45
+
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -24,7 +28,15 @@ def main() -> None:
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255,255,0))
     entities = {npc, player}
 
-    game_map = GameMap(map_width, map_get_height)
+    game_map = generate_dungeon(
+        max_rooms= max_rooms,
+        room_max_size= room_max_size,
+        room_min_size= room_min_size,
+        map_width= map_width,
+        map_height= map_height,
+        player=player
+    ) 
+    
     engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
 
     with tcod.context.new_terminal(
