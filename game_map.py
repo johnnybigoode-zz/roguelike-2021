b16c1f7879149054a36ea070bae31bf442e740ad
João A. Marson <jamarson@gmail.com>
2021-07-01 21:48:22 -0300

    Now with fov

--- game_map.py
+++ game_map.py
@@ -6,11 +6,22 @@ import tile_types
 class GameMap:
     def __init__(self, width: int, height: int):
         self.width, self.height = width, height
-        self.tiles = np.full((width, height), fill_value=tile_types.wall, order = "F")
-        
+        self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
+
+        self.visible = np.full((width, height), fill_value=False, order="F") #tiles the player can currently see
+        self.explored = np.full((width, height), fill_value=False, order="F") #tiles that have been seen
+
     def in_bounds(self, x: int, y: int) -> bool:
         """return true if x and y are inside of the bounds of this map"""
         return 0 <= x < self.width and 0 <= y < self.height
 
     def render(self, console: Console) -> None:
         console.tiles_rgb[0:self.width, 0:self.height] = self.tiles["dark"]
+        """
+        Render the map
+        """
+        console.tiles_rgb[0:self.width, 0:self.height] = np.select(
+            condlist=[self.visible, self.explored],
+            choicelist=[self.tiles["light"], self.tiles["dark"]],
+            default=tile_types.SHROUD
+        )
