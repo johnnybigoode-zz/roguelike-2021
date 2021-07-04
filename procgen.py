edaa672349f2328b4c4d827080d7c310432a0d7f
João A. Marson <jamarson@gmail.com>
2021-07-04 18:17:39 -0300

    Move entities group to gamemap

--- procgen.py
+++ procgen.py
@@ -65,7 +65,7 @@ def generate_dungeon(
     player: Entity, 
 ) -> GameMap:
     """generate a new dungeon map"""
-    dungeon = GameMap(map_width, map_height)
+    dungeon = GameMap(map_width, map_height, entities=[player])
 
     rooms: List[RectangularRoom] = []
 
