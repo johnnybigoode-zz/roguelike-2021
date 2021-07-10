f710558a354e4ee3fa69525006fe48a604321dcf
João A. Marson <jamarson@gmail.com>
2021-07-09 21:38:44 -0300

    Blind refactor

--- game_map.py
+++ game_map.py
@@ -7,19 +7,34 @@ from tcod.console import Console
 import tile_types
 
 if TYPE_CHECKING:
+    from engine import Engine
     from entity import Entity
+
 class GameMap:
-    def __init__(self, width: int, height: int, entities: Interable[Entity] = ()):
+    def __init__(
+        self, engine: Engine, width: int, height: int, entities: Iterable[Entity] = ()
+    ):
+        self.engine = engine
         self.width, self.height = width, height
         self.entities = set(entities)
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
 
-        self.visible = np.full((width, height), fill_value=False, order="F") #tiles the player can currently see
-        self.explored = np.full((width, height), fill_value=False, order="F") #tiles that have been seen
+        self.visible = np.full(
+            (width, height), fill_value=False, order="F"
+        ) #tiles the player can currently see
+        self.explored = np.full(
+            (width, height), fill_value=False, order="F"
+        ) #tiles that have been seen
 
-    def get_blocking_entity_at_location(self, location_x: int, location_y: int) -> Optional[Entity]:
+    def get_blocking_entity_at_location(
+        self, location_x: int, location_y: int
+    ) -> Optional[Entity]:
         for entity in self.entities:
-            if (entity.blocks_movement and entity.x == location_x and entity.y == location_y):
+            if (
+                entity.blocks_movement and 
+                entity.x == location_x and 
+                entity.y == location_y
+            ):
                 return entity
         
         return None
@@ -29,14 +44,13 @@ class GameMap:
         return 0 <= x < self.width and 0 <= y < self.height
 
     def render(self, console: Console) -> None:
-        console.tiles_rgb[0:self.width, 0:self.height] = self.tiles["dark"]
         """
         Render the map
         """
-        console.tiles_rgb[0:self.width, 0:self.height] = np.select(
+        console.tiles_rgb[0 : self.width, 0 : self.height] = np.select(
             condlist=[self.visible, self.explored],
             choicelist=[self.tiles["light"], self.tiles["dark"]],
-            default=tile_types.SHROUD
+            default=tile_types.SHROUD,
         )
 
         for entity in self.entities:
--- procgen.py
+++ procgen.py
@@ -10,7 +10,7 @@ from game_map import GameMap
 import tile_types
 
 if (TYPE_CHECKING):
-    from entity import Entity
+    from engine import Engine
 
 class RectangularRoom:
     def __init__(self, x: int, y: int, width: int, height: int):
@@ -79,10 +79,11 @@ def generate_dungeon(
     map_width: int, 
     map_height: int,
     max_monsters_per_room: int, 
-    player: Entity, 
+    engine: Engine, 
 ) -> GameMap:
     """generate a new dungeon map"""
-    dungeon = GameMap(map_width, map_height, entities=[player])
+    player = engine.player
+    dungeon = GameMap(engine, map_width, map_height, entities=[player])
 
     rooms: List[RectangularRoom] = []
 
@@ -106,7 +107,7 @@ def generate_dungeon(
 
         if (len(rooms) == 0):
             # the first room, where the player starts
-            player.x, player.y = new_room.center
+            player.place(*new_room.center, dungeon)
         else: # all other rooms
             #dig tunnel between this room and the previouis one
             for x, y in tunnel_between(rooms[-1].center, new_room.center):
