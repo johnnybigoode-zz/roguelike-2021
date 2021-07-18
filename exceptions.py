5bce04e2b52890d92807f82e26a008e9ec322c6e
João A. Marson <jamarson@gmail.com>
2021-07-18 20:31:59 -0300

    Add quit without saving exception

--- exceptions.py
+++ exceptions.py
@@ -1,4 +1,8 @@
 class Impossible(Exception):
     """exception raised when something impossible to be performed
-    
+
     the reason is given as the exception message"""
+
+
+class QuitWithoutSaving(SystemExit):
+    """can be raised to exit game without automatically saving"""
