c7016837e8239aa1c8af377ce7bb6545724dfb0b
João A. Marson <jamarson@gmail.com>
2021-07-20 14:17:20 -0300

    Can properly use stairs

--- actions.py
+++ actions.py
@@ -84,6 +84,14 @@ class WaitAction(Action):
     def perform(self) -> None:
         pass
 
+class TakeStairsAction(Action):
+    def perform(self) -> None:
+        if (self.entity.x, self.entity.y) == self.engine.game_map.downstairs_location:
+            self.engine.game_world.generate_floor()
+            self.engine.message_log.add_message(
+                f"You took the stairs down.")
+        else:
+            raise exceptions.Impossible("There are no stairs here.")
 
 class ActionWithDirection(Action):
     def __init__(self, entity: Actor, dx: int, dy: int):
